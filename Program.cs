6d6a5bbcd8092a361e57b4fcdde0f7abd9e21861
huyvu8051 <huyvu8051@gmail.com>
2021-08-10 23:56:16 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            List<Student> listStudent = NhapDSSinhVien();
+            /* List<Student> listStudent = new List<Student>();
+            for(int i = 10; i >= 0; i--)
+            {
+                Student s = new Student();
+                s.StudentID = i.ToString();
+                s.FullName = "Huy Vũ " + i;
+                s.AverageScore = i + 0.0f;
+                if(i % 2 == 0)
+                {
+                    s.Faculty = "CNTT";
+                }
+                else
+                {
+                    s.Faculty = "MMT";
+                }
+                listStudent.Add(s);
+            }
+            Student a = new Student();
+            a.StudentID = "11";
+            a.FullName = "Huy Vũ 11";
+            a.AverageScore = 10 + 0.0f;
+            a.Faculty = "CNTT";
+            listStudent.Add(a);*/
+
+            XuatDSSinhVien(listStudent);
+            do
+            {
+                Console.Write("\n-----MENU CHỨC NĂNG-----");
+                Console.Write("\n1. Xuất thông tin SV khoa CNTT");
+                Console.Write("\n2. Xuất thông tin SV điểm TB trên 5");
+                Console.Write("\n3. Xuất DSSV tăng dần theo điểm");
+                Console.Write("\n4. Xuất thông tin SV khoa CNTT, điểm TB trên 5");
+                Console.Write("\n5. Xuất thông tin SV khoa CNTT, điểm TB cao nhất");
+                Console.Write("\n0. Thoát");
+                Console.Write("\nNhập lựa chọn: ");
+                int luaChon = int.Parse(Console.ReadLine());
+
+                switch (luaChon)
+                {
+                    case 1:
+                        DSSVThuocCNTT(listStudent);
+                        break;
+                    case 2:
+                        DTBLonHonBang5(listStudent);
+                        break;
+                    case 3:
+                        SXSVDiemTangDan(listStudent);
+                        break;
+                    case 4:
+                        CNTTDTBLonHonBang5(listStudent);
+                        break;
+                    case 5:
+                        CNTTDTBCaoNhat(listStudent);
+                        break;
+                    default:
+                        if (luaChon == 0) return;
+                        break;
+                }
+
+            } while (true);
+        }
+
+        private static void CNTTDTBCaoNhat(List<Student> listStudents)
+        {
+            Console.WriteLine("\n 1.5 Xuất Danh Sách sinh viên thuộc khoa CNTT có điểm trung bình cao nhất");
+            float maximumScore = listStudents.OrderBy(s => s.AverageScore).Reverse().First().AverageScore;
+            List<Student> ls = (from s in listStudents
+                                where (s.Faculty == "CNTT" && s.AverageScore == maximumScore)
+                                select s).ToList();
+            if (ls.Count() == 0)
+            {
+                Console.WriteLine("Không có sinh viên thuộc khoa CNTT");
+            }
+            else
+            {
+                XuatDSSinhVien(ls);
+            }
+        }
+
+        private static void CNTTDTBLonHonBang5(List<Student> listStudents)
+        {
+            Console.WriteLine("\n 1.4 Xuất Danh Sách sinh viên thuộc khoa CNTT có điểm trung bình lớn hơn bằng 5");
+            List<Student> ls = (from s in listStudents
+                                where (s.Faculty == "CNTT" && s.AverageScore >= 5)
+                                select s).ToList();
+            if (ls.Count() == 0)
+            {
+                Console.WriteLine("Không có sinh viên thuộc khoa CNTT có điểm trung bình lớn hơn bằng 5");
+            }
+            else
+            {
+                XuatDSSinhVien(ls);
+            }
+        }
+
+        private static void DSSVThuocCNTT(List<Student> listStudents)
+        {
+            Console.WriteLine("\n 1.1 Xuất Danh Sách sinh viên thuộc khoa CNTT");
+            List<Student> ls = (from s in listStudents
+                                where s.Faculty == "CNTT"
+                                select s).ToList();
+            if (ls.Count() == 0)
+            {
+                Console.WriteLine("Không có sinh viên thuộc khoa CNTT");
+            }
+            else
+            {
+                XuatDSSinhVien(ls);
+            }
+        }
+
+        private static void DTBLonHonBang5(List<Student> listStudents)
+        {
+            Console.WriteLine("\n 1.2 Xuất ra thông tin các sinh viên có điểm TB lớn hơn bằng 5");
+            List<Student> listStudentResult = listStudents.Where(p => p.AverageScore >= 5).ToList();
+            if (listStudentResult.Count() == 0)
+                Console.WriteLine("Không có sinh viên nào có điểm >= 5");
+            else
+                XuatDSSinhVien(listStudentResult);
+        }
+
+        private static void SXSVDiemTangDan(List<Student> listStudents)
+        {
+            Console.WriteLine("\n 1.3 Sắp xếp sinh viên có điểm tăng dần");
+
+            List<Student> listStudentSort = listStudents.OrderBy(p => p.AverageScore).ToList();
+            XuatDSSinhVien(listStudentSort);
+        }
+
+        private static List<Student> NhapDSSinhVien()
+        {
+            List<Student> listStudents = new List<Student>();
+            string input = "";
+            int N = 0;
+            do
+            {
+                Console.Write("Nhập tổng số sinh viên N =");
+                input = Console.ReadLine();
+            } while (!IsNumber(input) || int.Parse(input) <= 0);
+            N = int.Parse(input);
+
+            Console.WriteLine("\n =====Nhập Danh Sách Sinh Viên=======");
+            for(int i = 0; i < N; i++)
+            {
+                Console.WriteLine("\n - Nhập sinh viên thứ {0}", i + 1);
+                Student temp = new Student();
+                temp.Input();
+                listStudents.Add(temp);
+            }
+            return listStudents;
+        }
+        private static void XuatDSSinhVien(List<Student> listStudent)
+        {
+            Console.WriteLine("====Xuất Danh Sách Sinh Viên====");
+            foreach(Student student in listStudent)
+            {
+                student.Show();
+            }
+        }
+        public static bool IsNumber(string s)
+        {
+            return Microsoft.VisualBasic.Information.IsNumeric(s);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab01
+{
+    class Student
+    {
+        //1. Tao thuoc tinh
+        private string studentID; //mã số sinh viên
+        private string fullName; // họ tên sinh viên
+        private float averageScore; //điểm trung bình
+        private string faculty; //khoa
+                                //2. Tao cac Property (tip: chọn Quick Actions And Refactorings từ thuộc tính)
+        public string StudentID
+        {
+            get
+            {
+                return studentID;
+            }
+            set
+            {
+                studentID = value;
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+            set
+            {
+                fullName = value;
+            }
+
+        }
+        public float AverageScore
+        {
+            get
+            {
+                return averageScore;
+            }
+            set
+            {
+                averageScore = value;
+            }
+        }
+        public string Faculty
+        {
+            get
+            {
+                return faculty;
+            }
+            set
+            {
+                faculty = value;
+            }
+        }
+        // 3. Tao constructor mặc định không tham số
+        public Student()
+        {
+        }
+        // 4. Tao constuctor có tham số
+        public Student(string id, string name, float score, string faculty)
+        {
+            StudentID = id;
+            FullName = name;
+            AverageScore = score;
+            Faculty = faculty;
+        }
+        // 5. Viết các phương thức nhập, xuất sinh viên
+        public void Input()
+        {
+            Console.Write("Nhập MSSV:");
+            StudentID = Console.ReadLine();
+            Console.Write("Nhập Họ tên Sinh viên:");
+            FullName = Console.ReadLine();
+
+            string input = "";
+            do
+            {
+                Console.Write("Nhập Điểm TB:");
+                input = Console.ReadLine();
+            } while (!IsNumber(input) || float.Parse(input) <= 0f);
+            
+            AverageScore = float.Parse(input); //ép sang kiểu float
+
+            Console.Write("Nhập Khoa:");
+            Faculty = Console.ReadLine();
+        }
+        public void Show()
+        {
+            Console.WriteLine("MSSV:{0} Họ Tên:{1} Khoa:{2} ĐiêmTB:{3}", this.StudentID,
+            this.fullName, this.Faculty, this.AverageScore);
+        }
+        public bool IsNumber(string s)
+        {
+            return Microsoft.VisualBasic.Information.IsNumeric(s);
+        }
+    }
+}
